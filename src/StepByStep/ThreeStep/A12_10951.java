39b3f55fce6a52cb0d0fe16235fb18640a4c6613
glory <dudrhkd6550@naver.com>
2023-10-11 22:30:08 +0900

    2023-10-11

--- src/StepByStep/ThreeStep/A12_10951.java
+++ src/StepByStep/ThreeStep/A12_10951.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 public class A12_10951 {
 
     public static void main(String[] args) throws IOException {
-        Scanner in=new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-        while(in.hasNextInt()){
-            int a=in.nextInt();
-            int b=in.nextInt();
-            System.out.println(a+b);
+        while (in.hasNextInt()) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println(a + b);
         }
     }
 }
